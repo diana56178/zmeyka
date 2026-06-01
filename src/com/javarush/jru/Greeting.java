14f8fa4dc63b207bad012c3adf69e69eaf231c3b
Diana <yusupova.diana.94@mail.ru>
2026-06-01 15:55:18 +0500

    FIX structure

---
+++ src/com/javarush/jru/Greeting.java
@@ -0,0 +1,7 @@
+package com.javarush.jru;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello, user!");
+    }
+}
---
+++ src/com/javarush/jru/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.javarush.jru;
+
+public class HelloWorld {
+    public static void main(String... arg) {
+        System.out.println("Hello World!");
+    }
+}
